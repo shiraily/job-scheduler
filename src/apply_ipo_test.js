adaedf8236cbcaf65f32e6ea91380f2ee867babd
Takaaki Shirai <lain.op.rai@gmail.com>
2020-11-19 23:28:47 +0900

    :construction: Apply SBI IPO

--- src/apply_ipo_test.js
+++ src/apply_ipo_test.js
@@ -1,8 +1,9 @@
 Feature('apply_ipo');
 
-Scenario('test something', ({ I }) => {
+Scenario('apply', ({ I }) => {
     I.amOnPage("https://www.sbisec.co.jp/");
     I.seeInCurrentUrl("ETGate");
     I.fillField("user_id", "user");
     I.fillField("user_password", "pass");
+    I.click("ログイン");
 });
