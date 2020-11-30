27e6e8f55ccb7f12df39f78c723872c6cce5bf2e
Takaaki Shirai <lain.op.rai@gmail.com>
2020-11-30 22:34:39 +0900

    Can apply SBI IPO

--- src/apply_ipo.js
+++ src/apply_ipo.js
@@ -1,14 +1,26 @@
 Feature('apply_ipo');
 
-[1].forEach(i => {
-    Scenario(`apply${i}`, ({ I }) => {
-        I.amOnPage("https://www.sbisec.co.jp/");
-        I.seeInCurrentUrl("ETGate");
-        I.fillField("user_id", process.env.SBI_USERNAME);
-        I.fillField("user_password", process.env.SBI_PASSWORD);
-        I.click("ACT_login");
-        I.see("お知らせ");
+const unit = 100;
+
+Scenario(`apply_ipo`, ({ I }) => {
+    I.amOnPage("https://www.sbisec.co.jp/");
+    I.seeInCurrentUrl("ETGate");
+    I.fillField("user_id", process.env.SBI_USERNAME);
+    I.fillField("user_password", process.env.SBI_PASSWORD);
+    I.click("ACT_login");
+    I.see("お知らせ");
+
+    let n = 0;
+    while (n < 20) {
         I.amOnPage("https://m.sbisec.co.jp/oeliw011?type=21");
-        I.see("IPOチャレンジポイント"); // TODO 申し込み ?
-    });
+        I.click("申込"); // TODO fallback
+
+        I.fillField("suryo", unit);
+        I.click("ストライクプライス");
+        I.fillField("tr_pass", process.env.SBI_TRADE_PASSWORD);
+        I.click("申込確認画面へ");
+
+        I.click("申込");
+        n += 1;
+    }
 });
