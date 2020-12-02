f1ddf668f7d979d90ff7fef3fc991ede4898c3a3
Takaaki Shirai <lain.op.rai@gmail.com>
2020-12-02 23:09:38 +0900

    fmt

--- src/apply_gotoeat_tokyo.js
+++ src/apply_gotoeat_tokyo.js
@@ -3,13 +3,13 @@ Feature('apply_gotoeat_tokyo');
 // TODO simplify
 
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].forEach(i => {
-    Scenario(`apply${i}`, ({ I }) => {
-        I.amOnPage("https://hajb.f.msgs.jp/webapp/form/23667_hajb_1/index.do");
-        I.fillField("address", process.env.EMAIL_ADDRESS);
-        I.fillField("domain", process.env.EMAIL_DOMAIN);
-        I.fillField("address2", process.env.EMAIL_ADDRESS);
-        I.fillField("domain2", process.env.EMAIL_ADDRESS);
-        I.click("確認画面へ");
-        I.click("申込みを確定する");
-    });
+  Scenario(`apply${i}`, ({ I }) => {
+    I.amOnPage("https://hajb.f.msgs.jp/webapp/form/23667_hajb_1/index.do");
+    I.fillField("address", process.env.EMAIL_ADDRESS);
+    I.fillField("domain", process.env.EMAIL_DOMAIN);
+    I.fillField("address2", process.env.EMAIL_ADDRESS);
+    I.fillField("domain2", process.env.EMAIL_ADDRESS);
+    I.click("確認画面へ");
+    I.click("申込みを確定する");
+  });
 })
--- src/apply_ipo.js
+++ src/apply_ipo.js
@@ -3,24 +3,24 @@ Feature('apply_ipo');
 const unit = 100;
 
 Scenario(`apply_ipo`, ({ I }) => {
-    I.amOnPage("https://www.sbisec.co.jp/");
-    I.seeInCurrentUrl("ETGate");
-    I.fillField("user_id", process.env.SBI_USERNAME);
-    I.fillField("user_password", process.env.SBI_PASSWORD);
-    I.click("ACT_login");
-    I.see("お知らせ");
+  I.amOnPage("https://www.sbisec.co.jp/");
+  I.seeInCurrentUrl("ETGate");
+  I.fillField("user_id", process.env.SBI_USERNAME);
+  I.fillField("user_password", process.env.SBI_PASSWORD);
+  I.click("ACT_login");
+  I.see("お知らせ");
 
-    let n = 0;
-    while (n < 20) {
-        I.amOnPage("https://m.sbisec.co.jp/oeliw011?type=21");
-        I.click("申込"); // TODO fallback
+  let n = 0;
+  while (n < 20) {
+    I.amOnPage("https://m.sbisec.co.jp/oeliw011?type=21");
+    I.click("申込"); // TODO fallback
 
-        I.fillField("suryo", unit);
-        I.click("ストライクプライス");
-        I.fillField("tr_pass", process.env.SBI_TRADE_PASSWORD);
-        I.click("申込確認画面へ");
+    I.fillField("suryo", unit);
+    I.click("ストライクプライス");
+    I.fillField("tr_pass", process.env.SBI_TRADE_PASSWORD);
+    I.click("申込確認画面へ");
 
-        I.click("申込");
-        n += 1;
-    }
+    I.click("申込");
+    n += 1;
+  }
 });
