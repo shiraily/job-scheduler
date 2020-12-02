484e1d5f91ce5662226be8300f99dcd4674584bc
Takaaki Shirai <lain.op.rai@gmail.com>
2020-12-03 03:02:20 +0900

    Replace CodeceptJS with Puppeteer

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # job-scheduler
+
 Personal job scheduler
+
+# Init
+
+### Firewall
+
+```
+gcloud app firewall-rules create 1 \
+--action=allow \
+--source-range='0.1.0.1' \
+--description='default -> deny, cron -> allow'
+```
+
+# Deploy
+
+```
+gcloud app deploy
+gcloud app deploy cron.yaml
+```
+
+# TODO
+
+* use dotenv
+
+etc...
--- src/apply_ipo.js
+++ src/apply_ipo.js
@@ -1,26 +1,52 @@
-Feature('apply_ipo');
+import puppeteer from "puppeteer";
 
+/** 取引単位（株） */
 const unit = 100;
 
-Scenario(`apply_ipo`, ({ I }) => {
-  I.amOnPage("https://www.sbisec.co.jp/");
-  I.seeInCurrentUrl("ETGate");
-  I.fillField("user_id", process.env.SBI_USERNAME);
-  I.fillField("user_password", process.env.SBI_PASSWORD);
-  I.click("ACT_login");
-  I.see("お知らせ");
+// TODO do not use waitForTimeout
+const _apply = (browser) => {
+  const page = await browser.newPage();
+  page.setViewport({width: 1280, height: 720});
+  await page.goto("https://www.sbisec.co.jp/");
+  await page.type("input[name=user_id]", process.env.SBI_USERNAME);
+  await page.type("input[name=user_password]", process.env.SBI_PASSWORD);
+
+  await page.click("input[name=ACT_login]");
 
   let n = 0;
-  while (n < 20) {
-    I.amOnPage("https://m.sbisec.co.jp/oeliw011?type=21");
-    I.click("申込"); // TODO fallback
+  while (n < 5) {
+    await page.waitForTimeout(1000);
+    await page.goto("https://m.sbisec.co.jp/oeliw011?type=21");
+    await page.waitForTimeout(1000);
+    let canApply = true;
+    await page.click("img[alt=申込]").catch( () => {canApply = false;});
+    if (!canApply) {
+      console.log("no BB");
+      break;
+    };
+    await page.waitForTimeout(1000);
 
-    I.fillField("suryo", unit);
-    I.click("ストライクプライス");
-    I.fillField("tr_pass", process.env.SBI_TRADE_PASSWORD);
-    I.click("申込確認画面へ");
+    await page.type("input[name=suryo]", unit.toString());
+    await page.click("#strPriceRadio");
+    await page.type("input[name=tr_pass]", process.env.SBI_TRADE_PASSWORD);
+    await page.click("input[name=order_kakunin]");
+    await page.waitForTimeout(1000);
 
-    I.click("申込");
+    await page.click("input[name=order_btn]");
     n += 1;
   }
-});
+}
+
+// TODO use Secret Manager instead of dotenv
+export const applyIPO = async () => {
+  const browser = await puppeteer.launch(
+    {
+      headless: false,
+    }
+  );
+  try {
+    _apply(browser);
+  } finally {
+    browser.close();
+  }
+};
