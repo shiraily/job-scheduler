30b822746ccc00f309d2254826d9c260329f2406
Takaaki Shirai <25303121+shiraily@users.noreply.github.com>
2023-02-11 17:33:35 +0900

    Update Shinsei Bank entry due to its name change

--- src/job/shinseiBankEntry.ts
+++ src/job/shinseiBankEntry.ts
@@ -6,9 +6,7 @@ import { JobHandler } from "./handler";
 export class ShinseiBankEntry extends JobHandler {
   async operate(): Promise<string> {
     console.log("start inputting Shinsei Bank entry");
-    await this.goto(
-      "https://webform.shinseibank.com/webapp/form/19913_xldb_4/index.do?lid=p"
-    );
+    await this.goto("https://webforms.sbishinseibank.co.jp/point-entry/input");
     await this.waitForType("#fi_account_num", process.env.SHINSEI_ACCOUNT);
     await this.page.type("#fi_birth_year", process.env.BIRTH_YEAR);
     await this.page.type("#fi_birth_month", process.env.BIRTH_MONTH);
@@ -18,7 +16,7 @@ export class ShinseiBankEntry extends JobHandler {
     await this.page.type("#fi_mail", process.env.EMAIL_ADDRESS);
 
     await this.page.click("label[for=fi_policy]");
-    await this.page.click("input[type=submit]");
+    await this.page.click("button[type=submit]");
 
     await this.page.waitForTimeout(6000);
     await this.page.click("input[value='エントリー']");
